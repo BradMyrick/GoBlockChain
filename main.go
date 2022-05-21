e0a1400e571faf465021ea2ad8cc3b777826f4ef
bradmyrick <brad.myrick@protonmail.com>
2022-05-20 19:49:41 -0500

    POW usage

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/BradMyrick/GoBlockChain/blockchain"
 )
@@ -14,9 +15,15 @@ func main() {
 	chain.AddBlock("Third Block after Genesis")
 
 	for _, block := range chain.Blocks {
+
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 
 	fmt.Printf("Blockchain Size: %d\n", chain.GetBlockchainSize())
